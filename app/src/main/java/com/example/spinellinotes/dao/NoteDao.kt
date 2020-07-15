3cbc9139736e2906fca663d9e9f3c61c2e8a6f8c
Vinicius <viinicius.sg@hotmail.com>
2020-07-14 23:37:03 -0300

    criando querys de sort

--- app/src/main/java/com/example/spinellinotes/dao/NoteDao.kt
+++ app/src/main/java/com/example/spinellinotes/dao/NoteDao.kt
@@ -3,7 +3,6 @@ package com.example.spinellinotes.dao
 import androidx.lifecycle.LiveData
 import androidx.room.*
 import com.example.spinellinotes.model.Note
-import io.reactivex.Flowable
 
 @Dao
 interface NoteDao{
@@ -11,11 +10,20 @@ interface NoteDao{
     @Query("SELECT * FROM note")
     fun getAllNotes(): LiveData<List<Note>>
 
+    @Query("SELECT * FROM note ORDER BY id DESC")
+    fun getAllNotesDesc(): LiveData<List<Note>>
+
+    @Query("SELECT * FROM note ORDER BY title")
+    fun getAllNotesAlpha(): LiveData<List<Note>>
+
+    @Query("SELECT * FROM note ORDER BY title DESC")
+    fun getAllNotesAlphaDesc(): LiveData<List<Note>>
+
     @Query("SELECT * FROM note WHERE id=:noteId LIMIT 1")
     fun getNoteById(noteId: Long) : Note
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    fun addNote(note: Note)
+    fun addNote(note: Note) : Long
 
     @Update
     fun updateNote(note: Note)
